93b849a7f7abc211424c092f356e43bbb75094b5
Yuxiao QIN <hi@aprilab.dev>
2024-01-02 23:07:44 +0800

    the first implemnt of FC1 data in doris.

--- src/slcimage.cc
+++ src/slcimage.cc
@@ -774,6 +774,12 @@ void slcimage::fillslcimage(const char* file)
           DEBUG.print("Substring \"GF3\" found in Product type specifier.");
           sensor = SLC_GF3;
         }
+      pch = strstr(word,"chaohu-1");
+      if (pch != NULL)  // FC1
+        {
+          DEBUG.print("Substring \"FC1\" found in Product type specifier.");
+          sensor = SLC_FC1;
+        }
       pch = strstr (word,"TSX");
       if (pch != NULL)
         {
