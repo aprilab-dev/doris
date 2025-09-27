4855ffe653f8aa506660590c9fe5ded7e7e59d6d
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2025-09-27 23:08:28 +0800

    added support for bc4

--- bin/bc3_dump_header2doris.py
+++ bin/bc3_dump_header2doris.py
@@ -15,17 +15,16 @@ from datetime import datetime, timedelta
 
 SPEED_OF_LIGHT = 299792458
 
-
-def locate(pattern: str, root=os.curdir) -> str:
-    # region docstring
-    """Locate the **first** file matching supplied filename pattern
+def locate(pattern, root=os.curdir) -> str:
+    """Locate the **first** file matching supplied filename pattern(s)
     in and below supplied root directory.
 
     Parameters
     ----------
-    pattern : str
-        The pattern that you're looking for. The pattern follows the same rule
-        as in linux system, as "*" is allowed.
+    pattern : str or tuple
+        The pattern(s) that you're looking for. The pattern follows the same rule
+        as in linux system, as "*" is allowed. If a tuple is provided, the function
+        will search for files matching any of the patterns in the tuple.
     root : str, optional
         the root directory for searching the pattern, by default os.curdir.
 
@@ -39,14 +38,22 @@ def locate(pattern: str, root=os.curdir) -> str:
     You can use either "return" or "yield", but be aware of the diference
     between the two.
     """
-    # endregion
+    # 如果 pattern 是字符串，转换为元组以便统一处理
+    if isinstance(pattern, str):
+        patterns = (pattern,)
+    else:
+        patterns = pattern
 
-    # TODO: consider using os.getcwd()?
-    # see https://stackmirror.com/questions/14512087
-    for path, dirs, files in os.walk(os.path.abspath(root), followlinks=True):
-        for filename in fnmatch.filter(files, pattern):
-            return os.path.join(path, filename)
-    raise FileNotFoundError
+    # 遍历目录
+    for path, _, files in os.walk(os.path.abspath(root), followlinks=True):
+        for filename in files:
+            # 检查文件名是否匹配任何一个 pattern
+            for pat in patterns:
+                if fnmatch.fnmatch(filename, pat):
+                    return os.path.join(path, filename)
+
+    # 如果没有找到匹配的文件，抛出异常
+    raise FileNotFoundError(f"No file found matching any of the patterns: {patterns}")
 
 
 def hms2sec(hmsString, convertFlag="int"):
@@ -91,7 +98,7 @@ class BC3:
         directory : str
             [description]
         """
-        pattern = "bc3-sm-slc*.xml"
+        pattern = ("bc3-sm-slc*.xml", "bc4-sm-slc*.xml", "bc5-sm-slc*.xml")
         self.meta["path"] = locate(pattern, directory)
 
         return self
@@ -201,7 +208,7 @@ class BC3:
         )
 
         container["Datafile"] = os.path.basename(
-            locate("bc3*slc*.tiff", os.path.dirname(self.meta["path"]))
+            locate(("bc3*slc*.tiff", "bc4*slc*.tiff", "bc5*slc*.tiff"), os.path.dirname(self.meta["path"]))
         )
 
         # 2-way Slant Range Time
